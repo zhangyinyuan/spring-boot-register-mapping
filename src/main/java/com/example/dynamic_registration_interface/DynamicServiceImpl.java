8b12b8d6298d23b3223153194f290472c4a71c28
hq01uh176 <>
2025-01-02 16:06:35 +0800

    引入<artifactId>spring-boot-starter-actuator</artifactId>依赖后. 动态注册的方法无法识别, 解决这一冲突的办法是 management.endpoints.web.exposure.exclude=metrics

--- src/main/java/com/example/dynamic_registration_interface/DynamicServiceImpl.java
+++ src/main/java/com/example/dynamic_registration_interface/DynamicServiceImpl.java
@@ -19,51 +19,48 @@ public class DynamicServiceImpl implements DynamicService {
 
     @Autowired
     @Qualifier("requestMappingHandlerMapping")
-    private RequestMappingHandlerMapping requestMappingHandlerMapping;
+    private RequestMappingHandlerMapping handlerMapping;
 
     @Override
     public void register(UserLoginReq userLoginReq) throws NoSuchMethodException {
-
         // 无参get方法
         RequestMappingInfo requestMappingInfo = RequestMappingInfo.paths("/dynamic/lmcTest").methods(RequestMethod.GET).build();
-        requestMappingHandlerMapping.registerMapping(requestMappingInfo, "adapterController", AdapterController.class.getDeclaredMethod("myTest"));
+        handlerMapping.registerMapping(requestMappingInfo, "adapterController", AdapterController.class.getDeclaredMethod("myTest"));
 
         // 带一参数的get方法
         RequestMappingInfo requestMappingInfo1 = RequestMappingInfo.paths("/dynamic/lmcTest2").params("fileName").methods(RequestMethod.GET).build();
-        requestMappingHandlerMapping.registerMapping(requestMappingInfo1, "adapterController", AdapterController.class.getDeclaredMethod("myTest2", String.class));
+        handlerMapping.registerMapping(requestMappingInfo1, "adapterController", AdapterController.class.getDeclaredMethod("myTest2", String.class));
 
         // 带多个参数的get方法
         RequestMappingInfo requestMappingInfo2 = RequestMappingInfo.paths("/dynamic/lmcTest3")
                 .params("fileName", "type", "isSort")
                 .methods(RequestMethod.GET).build();
-        requestMappingHandlerMapping.registerMapping(requestMappingInfo2, "adapterController", AdapterController.class.getDeclaredMethod("myTest3", String.class, String.class, Boolean.class));
+        handlerMapping.registerMapping(requestMappingInfo2, "adapterController", AdapterController.class.getDeclaredMethod("myTest3", String.class, String.class, Boolean.class));
 
         // 无参post方法
         RequestMappingInfo requestMappingInfo3 = RequestMappingInfo.paths("/dynamic/lmcTest4").methods(RequestMethod.POST).build();
-        requestMappingHandlerMapping.registerMapping(requestMappingInfo3, "adapterController", AdapterController.class.getDeclaredMethod("myTest"));
+        handlerMapping.registerMapping(requestMappingInfo3, "adapterController", AdapterController.class.getDeclaredMethod("myTest"));
 
         // 带参post方法
         RequestMappingInfo requestMappingInfo4 = RequestMappingInfo.paths("/dynamic/lmcTest5")
                 .params("fileName", "type", "isSort")
                 .methods(RequestMethod.POST).build();
-        requestMappingHandlerMapping.registerMapping(requestMappingInfo4, "adapterController", AdapterController.class.getDeclaredMethod("myTest3", String.class, String.class, Boolean.class));
-
+        handlerMapping.registerMapping(requestMappingInfo4, "adapterController", AdapterController.class.getDeclaredMethod("myTest3", String.class, String.class, Boolean.class));
 
         // body带参的post方法
         RequestMappingInfo requestMappingInfo5 = RequestMappingInfo.paths("/dynamic/lmcTest6")
                 .produces("application/json;charset=UTF-8")
                 .methods(RequestMethod.POST).build();
-        requestMappingHandlerMapping.registerMapping(requestMappingInfo5, "adapterController", AdapterController.class.getDeclaredMethod("myTest4", HttpServletRequest.class));
-
+        handlerMapping.registerMapping(requestMappingInfo5, "adapterController", AdapterController.class.getDeclaredMethod("myTest4", HttpServletRequest.class));
 
         //get请求多个动态参数
         RequestMappingInfo requestMappingInfo6 = RequestMappingInfo.paths("/dynamic/lmcTest7")
                 .produces("application/json;charset=UTF-8")
                 .methods(RequestMethod.GET).build();
-        requestMappingHandlerMapping.registerMapping(requestMappingInfo6, "adapterController", AdapterController.class.getDeclaredMethod("handleDynamicParams", Map.class));
+        handlerMapping.registerMapping(requestMappingInfo6, "adapterController", AdapterController.class.getDeclaredMethod("handleDynamicParams", Map.class));
 
-        // 手动刷新 HandlerMapping 缓存
-        requestMappingHandlerMapping.afterPropertiesSet();
+        // afterPropertiesSet会清空所有非Actuator的静态接口.慎用
+        // handlerMapping.afterPropertiesSet();
     }
 
     @Override
--- src/main/java/com/example/dynamic_registration_interface/RegisterController.java
+++ src/main/java/com/example/dynamic_registration_interface/RegisterController.java
@@ -1,14 +1,26 @@
 package com.example.dynamic_registration_interface;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @RestController
 public class RegisterController {
 
     @Autowired
     private DynamicService dynamicService;
+
+    @Autowired @Qualifier("requestMappingHandlerMapping")
+    private RequestMappingHandlerMapping handlerMapping;
 
     @GetMapping("/register")
     public Object register(UserLoginReq userLoginReq) throws NoSuchMethodException {
@@ -22,4 +34,16 @@ public class RegisterController {
         return "unregister [" + apiName + "] success";
     }
 
+    @GetMapping("/registeredMethods")
+    public List<String> registeredMethods() {
+        List<String> registeredMethods = new LinkedList<>();
+        Set<Map.Entry<RequestMappingInfo, HandlerMethod>> entries = handlerMapping.getHandlerMethods().entrySet();
+        for (Map.Entry<RequestMappingInfo, HandlerMethod> entry : entries) {
+            RequestMappingInfo info = entry.getKey();
+            if (info.getPatternsCondition() != null) {
+                registeredMethods.add(info.getPatternsCondition().toString());
+            }
+        }
+        return registeredMethods;
+    }
 }
